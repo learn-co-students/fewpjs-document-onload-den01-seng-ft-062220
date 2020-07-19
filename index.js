ff599e72258de62e4d5515f8db39045a2544014f
mnavoy4 <mnavoy4@gmail.com>
2020-07-19 11:08:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  let targetP = document.querySelector("#text");
+  targetP.textContent = "This is really cool!";
+})
